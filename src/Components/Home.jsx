4be608d77b907e210cb1ca64030ceb813487dbdd
Will Olson <will.olson.b@gmail.com>
2024-11-08 12:43:17 -0600

    Updated home divs and overview.js file. Pending component buildout for standalone pages.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,14 +2,52 @@ import React from 'react'
 import Page from './Page'
 import Header from './Header'
 import Navbar from './Navbar'
+import overview from './Overview'
 
-function Home(){
+function Home() {
+    console.log(overview.AI.Overview)
     return (
         <div>
             <Header />
             <Navbar />
+
+            <div className="overview"> 
+                <h2>Site Overview Placeholder</h2>
+            </div>
+
+            <div className="BigTechHeader">
+                <h2>{overview.BigTech.Overview}</h2>
+                <h2>{overview.Media.Overview}</h2>
+                <h2>{overview.AI.Overview}</h2>
+            </div>
+
+            <div className="SaaS">
+                <h2>{overview.MarketingSaaS.Overview}</h2>
+                <h2>{overview.B2BSaaS.Overview}</h2>
+            </div>
+
+            <div className="Finance">
+                <h2>{overview.Fintech.Overview}</h2>
+                <h2>{overview.Finance.Overview}</h2>
+            </div>
+
+            <div className="HealthcareHeader">
+                <h2>{overview.Healthcare.Overview}</h2>
+            </div>
+
+            <div className="IndustryLeadersHeader">
+                <h2>{overview.IndustryLeaders.Overview}</h2>
+            </div>
+
+            <div className="MostFavoritedHeader">
+                <h2>{overview.MostFavorited.Overview}</h2>
+            </div>
+
+            <div className="CommunitySubmissionHeader">
+                <h2>{overview.CommunitySubmissions.Overview}</h2>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
---
+++ src/Components/Overview.js
@@ -0,0 +1,48 @@
+const overview =
+{
+"BigTech":
+    {
+      "Overview": "test"
+    },
+"Media":
+    {
+      "Overview": "test"
+    },
+"AI": 
+    {
+      "Overview": "test"
+    },
+"MarketingSaaS":
+    {
+      "Overview": "test"
+    },
+"B2BSaaS":
+    {
+      "Overview": "test"
+    },
+"Fintech":
+    {
+      "Overview": "test"
+    },
+"Finance":
+    {
+      "Overview": "test"
+    },
+"Healthcare":
+    {
+      "Overview": "test"
+    },
+"IndustryLeaders":
+    {
+      "Overview": "test"
+    },
+"CommunitySubmissions":
+    {
+      "Overview": "test"
+    },
+"MostFavorited":
+    {
+      "Overview": "test"
+    }
+}
+export default overview
