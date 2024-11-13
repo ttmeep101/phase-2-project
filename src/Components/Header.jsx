7a20b582fd42fb1befbdb98b23ba3285148b677f
Will Olson <will.olson.b@gmail.com>
2024-11-13 10:34:42 -0600

    Minor code update.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 function Header() {
-  // Initialize with the value from localStorage or default to false
   const [isDarkMode, setIsDarkMode] = useState(() => {
     return localStorage.getItem('isDarkMode') === 'true';
   });
 
   useEffect(() => {
-    // Apply the appropriate class to the body for styling
     document.body.className = isDarkMode ? 'dark-mode' : 'light-mode';
 
-    // Save the current theme preference in localStorage
     localStorage.setItem('isDarkMode', isDarkMode);
   }, [isDarkMode]);
 
   function toggleDarkMode() {
-    // Switch the dark mode state
     setIsDarkMode(prevMode => !prevMode);
   }
 
